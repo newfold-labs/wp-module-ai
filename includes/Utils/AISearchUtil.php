fd680a586db3b8a87af11b1cbb8708b0cc397da1
amartya-dev <amarkaushik1999@gmail.com>
2023-04-26 21:26:41 +0530

    lint

--- includes/Utils/AISearchUtil.php
+++ includes/Utils/AISearchUtil.php
@@ -11,32 +11,37 @@ class AISearchUtil {
 	/**
 	 * The function to proxy to the AI service and get a response
 	 *
-	 * @param string $hiive_token
-	 * @param string $user_prompt
-	 * @param string $identifier
-	 * @param array|null $extra
+	 * @param string     $hiive_token The Hive token
+	 * @param string     $user_prompt The user search query
+	 * @param string     $identifier  The identifier for the caller
+	 * @param array|null $extra       Extra parameters to be included
 	 * @return array
 	 */
-	public static function get_search_results (
+	public static function get_search_results(
 		string $hiive_token, string $user_prompt, string $identifier, array $extra = null
 	) {
 		if ( ! function_exists( 'wp_json_encode' ) ) {
 			require_once ABSPATH . 'wp-includes/functions.php';
 		}
 
-		$response = wp_remote_post( AI_SERVICE_BASE , array(
-			'method'  => 'POST',
-			'headers' => array(
-				'Content-Type' => 'application/json'
-			),
-			'timeout' => 60,
-			'body'    => wp_json_encode( array(
-				'hiivetoken' => $hiive_token,
-				'prompt'     => $user_prompt,
-				'identifier' => $identifier,
-				'extra'      => $extra
-			) )
-		) );
+		$response = wp_remote_post(
+			AI_SERVICE_BASE,
+			array(
+				'method'  => 'POST',
+				'headers' => array(
+					'Content-Type' => 'application/json'
+				),
+				'timeout' => 60,
+				'body'    => wp_json_encode(
+					array(
+						'hiivetoken' => $hiive_token,
+						'prompt'     => $user_prompt,
+						'identifier' => $identifier,
+						'extra'      => $extra
+					)
+				)
+			)
+		);
 		if ( wp_remote_retrieve_response_code( $response ) !== 200 ) {
 			return array(
 				'error' => __('We are unable to process the request at this moment')
