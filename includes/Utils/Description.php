f783ec01fc6d6c5e97d73e48c757e2de0ec5bd4c
Ajay Yadav <ajay.yadav@newfold.com>
2023-07-27 19:07:17 +0530

    Press3-84

--- includes/Utils/Description.php
+++ includes/Utils/Description.php
@@ -6,8 +6,8 @@ class Description {
     public function set_description_container() {
         $plugin_url = BLUEHOST_PLUGIN_URL;
         $script_url = $plugin_url . '/vendor/newfold-labs/wp-module-onboarding/vendor/newfold-labs/wp-module-ai/dist/index.js';
-        /* $script_url = '../../dist/index.js';
-        wp_enqueue_script('custom-plugin-script-description', $script_url, array(), '1.0', true); */
+        /* $script_url = '../../dist/index.js'; */
+        wp_enqueue_script('custom-plugin-script-description', $script_url, array(), '1.0', true); 
         ?>
             <div id="description-generator-container"></div>
             <script type='text/javascript'>
