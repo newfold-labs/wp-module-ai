e87cb3f9d871193259aa2ced7eb9b77ed8ea38ee
Ajay Yadav <ajay.yadav@newfold.com>
2023-08-10 19:45:42 +0530

    press3-89

--- includes/Utils/Description.php
+++ includes/Utils/Description.php
@@ -4,7 +4,7 @@ namespace NewfoldLabs\WP\Module\AI\Utils;
 
 class Description {
     public function set_description_container() {
-        wp_enqueue_script('custom-plugin-script-description', '../../dist/index.js', array(), '1.0', true);
+        $script_url = plugins_url('../../dist/index.js', __FILE__);
         wp_enqueue_script('custom-plugin-script-description', $script_url, array(), '1.0', true);
         global $pagenow;
 
@@ -30,7 +30,8 @@ class Description {
         if ($pagenow !== 'post-new.php' && $pagenow !== 'post.php') {
             return;
         }
-        wp_enqueue_script('custom-gutenberg-plugin-script', '../../dist/index.js', array('wp-edit-post', 'wp-plugins', 'wp-element', 'wp-components'), '1.0', true);
+        $script_url = plugins_url('../../dist/index.js', __FILE__);
+        wp_enqueue_script('custom-gutenberg-plugin-script', $script_url , array('wp-edit-post', 'wp-plugins', 'wp-element', 'wp-components'), '1.0', true);
     }
     
     public function description_admin_init() {
