8cd865fd6b8bcd0a1680065faaa617ad4defad5a
Ajay Yadav <ajay.yadav@newfold.com>
2023-08-10 12:57:56 +0530

    press3-89

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '617eaf4c1ac1cbe41daf');
+<?php return array('dependencies' => array('wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '590a6f737b150f6bb824');
