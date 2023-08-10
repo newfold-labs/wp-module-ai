97f1d1d289b8ce4e1884c70b6544477549992134
Ajay Yadav <ajay.yadav@newfold.com>
2023-08-11 01:04:08 +0530

    press3-89

--- includes/Utils/Description.php
+++ includes/Utils/Description.php
@@ -3,25 +3,27 @@
 namespace NewfoldLabs\WP\Module\AI\Utils;
 
 class Description {
+    private $script_url;
+
+    public function __construct() {
+        $this->script_url = plugins_url('../../dist/index.js', __FILE__);
+    }
+
     public function set_description_container() {
-        $script_url = plugins_url('../../dist/index.js', __FILE__);
-        wp_enqueue_script('custom-plugin-script-description', $script_url, array(), '1.0', true);
+        wp_enqueue_script('custom-plugin-script-description', $this->script_url, array(), '1.0', true);
         global $pagenow;
 
         if ($pagenow == 'options-general.php') {
-            ?>
-            <div id="description-generator-container"></div>
-            <script type='text/javascript'>
-                jQuery(document).ready(function($) {
-                    $("#description-generator-container").insertAfter($("#blogdescription").closest("tr"));
-                });
-            </script>;
-        <?php
+            echo '<div id="description-generator-container"></div>';
+            echo '<script type="text/javascript">
+                    jQuery(document).ready(function($) {
+                        $("#description-generator-container").insertAfter($("#blogdescription").closest("tr"));
+                    });
+                  </script>';
         }
+
         if ($pagenow == 'post-new.php' || $pagenow == 'post.php') {
-            ?>
-            <div id="description-generator-container"></div>
-        <?php
+            echo '<div id="description-generator-container"></div>';
         }
     }
 
@@ -30,13 +32,12 @@ class Description {
         if ($pagenow !== 'post-new.php' && $pagenow !== 'post.php') {
             return;
         }
-        $script_url = plugins_url('../../dist/index.js', __FILE__);
-        wp_enqueue_script('custom-gutenberg-plugin-script', $script_url , array('wp-edit-post', 'wp-plugins', 'wp-element', 'wp-components'), '1.0', true);
+
+        wp_enqueue_script('custom-gutenberg-plugin-script', $this->script_url, array('wp-edit-post', 'wp-plugins', 'wp-element', 'wp-components'), '1.0', true);
     }
     
     public function description_admin_init() {
         add_action('enqueue_block_editor_assets', array($this, 'enqueue_custom_gutenberg_plugin'));
-        // Hook the set_description_container method to the admin_footer action
         add_action('admin_footer', array($this, 'set_description_container'));
     }
 }
